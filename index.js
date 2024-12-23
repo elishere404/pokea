cff456ed0e369c91e0ce10458b2a00f9c4671232
elishere404 <misterbombastic@gmail.com>
2024-12-24 02:03:18 +0400

    Initial deployment

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const path = require('path');
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const currencySymbols = {
+    'USD': '$',
+    'EUR': '€',
+    'GBP': '£',
+    'JPY': '¥',
+    'AUD': 'A$',
+    'CAD': 'C$',
+    'CHF': 'CHF',
+    'CNY': '¥',
+    'INR': '₹',
+    'NZD': 'NZ$'
+};
+
+function getCurrencySymbol(currencyCode) {
+    return currencySymbols[currencyCode] || currencyCode;
+}
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+const productsData = require('./data/products.json');
+const favicon = require('serve-favicon');
+
+app.locals.getCurrencySymbol = getCurrencySymbol;
+
+app.use(favicon(path.join(__dirname, 'favicon.ico')));
+
+app.get('/', (req, res) => {
+    res.redirect('/products');
+});
+
+app.get('/products', (req, res) => {
+    res.render('products', { 
+        products: productsData.products,
+        paypalClientId: process.env.PAYPAL_CLIENT_ID || '',
+        currency: process.env.CURRENCY || 'EUR'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.use((req, res) => {
+    res.status(200).send(':(, something bad happened ');
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+}); 
